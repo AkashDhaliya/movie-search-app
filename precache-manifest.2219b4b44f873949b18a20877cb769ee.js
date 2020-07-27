c1b07ff207372b5eb5d4cb023476eb4607f97e9f
Akash Kumar <Akash.dhaliya@gmail.com>
2020-07-27 10:24:22 +0530

    Updates

---
+++ precache-manifest.2219b4b44f873949b18a20877cb769ee.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "81d72229aa99a3997cf26a62911b1836",
+    "url": "/movie-search-app/index.html"
+  },
+  {
+    "revision": "52a445204127a9e87d73",
+    "url": "/movie-search-app/static/css/main.f22eec42.chunk.css"
+  },
+  {
+    "revision": "a90ceb1670f3e6132cfc",
+    "url": "/movie-search-app/static/js/2.0009aee9.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/movie-search-app/static/js/2.0009aee9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "52a445204127a9e87d73",
+    "url": "/movie-search-app/static/js/main.b04f4c2a.chunk.js"
+  },
+  {
+    "revision": "93854698e18f7c10401e",
+    "url": "/movie-search-app/static/js/runtime-main.fb6de319.js"
+  }
+]);
